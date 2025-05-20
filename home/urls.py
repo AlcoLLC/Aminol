b885f8cdcff1476e0b0f05b6fd4f9687acc76b68
Aygun Amir <aygun.amiraslanovaa@gmail.com>
2025-05-20 15:45:22 +0400

    Contact Completed

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
+<<<<<<< HEAD
 from .views import home_view, service_aminol_dealer_view, service_laboratory_view, service_logistics_view, markets_automotive_view, markets_industrial_view, markets_shipping_view, contact_view
+=======
+from .views import home_view, markets_automotive_view, markets_industrial_view, markets_shipping_view
+>>>>>>> f376d9a5a1ed5d56cba2a883510474d7facae5e8
 
 urlpatterns = [
     path("", home_view),
-    path('service_aminol_dealer/', service_aminol_dealer_view,
-         name='service_aminol_dealer'),
-    path('service_laboratory/', service_laboratory_view, name='service_laboratory'),
-    path('service_logistics/', service_logistics_view, name='service_logistics'),
     path('markets_automotive/', markets_automotive_view, name='markets_automotive'),
     path('markets_industrial/', markets_industrial_view, name='markets_industrial'),
     path('markets_shipping/', markets_shipping_view, name='markets_shipping'),
