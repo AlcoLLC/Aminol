864091a23d967d44e43cfad97d45b0b94bf4e1fc
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2025-05-29 15:57:52 +0400

    logos

--- home/models.py
+++ home/models.py
@@ -23,3 +23,12 @@ class Gallery(models.Model):
 
     def __str__(self):
         return f"Gallery Image {self.id}"
+
+class Supplier(models.Model):
+    image = models.ImageField(upload_to='supplier/')
+    order = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Gallery Image {self.id}"
