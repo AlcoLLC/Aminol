e51a77ce62f0107f4834e533f339628aa9071433
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-06-03 10:23:13 +0400

    career

--- career/urls.py
+++ career/urls.py
@@ -5,6 +5,7 @@ app_name = 'career'
 
 urlpatterns = [
     path('career/', views.career_view, name='career'),
-    path('career_steps/', views.career_steps_view, name='career_steps'),
+    path('apply/<int:job_id>/', views.career_steps_view, name='career_steps'),
+    path('application-step/', views.career_application_step, name='application_step'),
+    path('filter-jobs/', views.filter_jobs_by_department, name='filter_jobs'),
 ]
-
