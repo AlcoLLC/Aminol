b8e70bbcb262c29e6dc499d2285494621db46ad6
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-06-27 12:28:39 +0400

    seo

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import home_view
 
 app_name = "home"
+
 urlpatterns = [
     path("", home_view, name="home"),
     # path('career/', career_view, name='career'),
