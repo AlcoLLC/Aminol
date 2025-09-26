6c15d366ac4a7ef63899863cdc259d1da2426901
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-09-26 16:06:31 +0400

    about-section

--- staticfiles/assets/js/about.js
+++ staticfiles/assets/js/about.js
@@ -165,23 +165,5 @@ document.addEventListener("DOMContentLoaded", function () {
       }, 300);
     }
   });
-
-  document.addEventListener("DOMContentLoaded", function() {
-  const animatedSections = document.querySelectorAll('.section-row');
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-        observer.unobserve(entry.target);
-      }
-    });
-  }, {
-    threshold: 0.1 
-  });
-
-  animatedSections.forEach(section => {
-    observer.observe(section);
-  });
-});
+  
 });
