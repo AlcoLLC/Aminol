320a4ba8dad79e3d6bcfd35d7376d4f7f57ff2fb
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-09-29 15:24:19 +0400

    product-group

--- staticfiles/assets/js/locale.js
+++ staticfiles/assets/js/locale.js
@@ -138,10 +138,10 @@ document.addEventListener("DOMContentLoaded", function () {
           // Eğer evetse, arkaplan yüksekliğini 350px yap
           if (window.innerWidth <= 1100) {
             // Eğer ekran 1100px veya daha darsa, yüksekliği 385px yap
-            dropdownBackground.style.height = "360px";
+            dropdownBackground.style.height = "385px";
         } else {
             // Daha geniş ekranlar için yüksekliği 350px yap
-            dropdownBackground.style.height = "350px";
+            dropdownBackground.style.height = "320px";
         }
         } else {
           // Değilse, yüksekliği sıfırla (veya varsayılan bir değere ayarla)
