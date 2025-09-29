e91f71310e9efc1e4737d65a55c82d9c4b1bcd37
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-09-29 14:43:27 +0400

    product-group

--- staticfiles/assets/js/locale.js
+++ staticfiles/assets/js/locale.js
@@ -136,11 +136,17 @@ document.addEventListener("DOMContentLoaded", function () {
         // Üzerine gelinen dropdown'un "products-dropdown" class'ına sahip olup olmadığını kontrol et
         if (this.classList.contains('products-dropdown')) {
           // Eğer evetse, arkaplan yüksekliğini 350px yap
-          dropdownBackground.style.height = "300px";
+          if (window.innerWidth <= 1100 && window.innerWidth > 1000) {
+            // 1000 < width ≤ 1100
+            dropdownBackground.style.height = "385px";
+        } else if (window.innerWidth > 1100) {
+            // width > 1100
+            dropdownBackground.style.height = "350px";
+        } 
         } else {
-          // Değilse, yüksekliği sıfırla (veya varsayılan bir değere ayarla)
-          dropdownBackground.style.height = "";
-        }
+    // width ≤ 1000
+    dropdownBackground.style.height = "320px";
+}
         // YENİ EKLENEN KOD SONU
 
         dropdownBackground.style.display = "block";
