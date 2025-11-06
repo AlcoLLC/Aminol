bf5f0f59e654119e33377f1d173870fe39efa6da
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-11-06 16:53:22 +0400

    seo

--- home/views.py
+++ home/views.py
@@ -48,3 +48,7 @@ def handler400(request, exception):
     400 Error Handler
     """
     return render(request, '400.html', status=400)
+
+def handler405(request, exception=None):
+    response = render(request, "405.html", status=405)
+    return response
